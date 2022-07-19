1770ebc115dd2414f581b1f8996bad62724d1f5a
sunjung0120 <sspure123@naver.com>
2022-07-19 13:43:27 +0900

    spring_project

--- hello_spring/hello_spring/src/main/java/hello_spring/hello_spring/SpringConfig.java
+++ hello_spring/hello_spring/src/main/java/hello_spring/hello_spring/SpringConfig.java
@@ -1,13 +1,21 @@
 package hello_spring.hello_spring;
 
+import hello_spring.hello_spring.repository.JdbcTempleteMemberRepository;
 import hello_spring.hello_spring.repository.MemberRepository;
 import hello_spring.hello_spring.repository.MemoryMemberRepository;
 import hello_spring.hello_spring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import hello_spring.hello_spring.repository.JdbcMemberRepository;
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+
+    DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource; }
 
     @Bean
     public MemberService memberService(){
@@ -16,7 +24,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTempleteMemberRepository(dataSource);
     }
 
 }
